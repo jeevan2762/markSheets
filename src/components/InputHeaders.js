f73e12b821bb4d1be938b706c9f556d0f53317c8
jeevan kumar <jjjeeevvvaaan@gmail.com>
2023-01-25 12:56:24 +0530

    Deleted EmptyCells Component.

--- src/components/InputHeaders.js
+++ src/components/InputHeaders.js
@@ -1,14 +1,10 @@
 import { React } from 'react';
 import Add from './Buttons/Add';
-import EmptyCell from './EmptyCell';
 import InputBoxes from './Input/InputBoxes';
 
 const InputHeaders = (context) =>
 	<tr>
 		<InputBoxes { ...context }/>
-		<EmptyCell/>
-		<EmptyCell/>
-		<EmptyCell/>
 		<td>
 			<Add { ...context }/>
 		</td>
